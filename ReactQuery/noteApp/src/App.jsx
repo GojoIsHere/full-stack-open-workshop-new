13c0f62eacb196a0b0ab22d7a9f6c6d41af5cd36
GojoIsHere <sus.thanet0726@gmail.com>
2023-09-15 14:14:45 +0545

    added and deleted some

--- ReactQuery/noteApp/src/App.jsx
+++ ReactQuery/noteApp/src/App.jsx
@@ -1,16 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const App = () => {
-  const addNote = async (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    console.log(content);
-  };
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/notes").then((res) => res.data),
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
 
-  const toggleImportance = (note) => {
-    console.log("toggle importance of", note.id);
-  };
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
 
-  const notes = [];
+  const notes = result.data;
+
+  // const addNote = async (event) => {
+  //   event.preventDefault();
+  //   const content = event.target.note.value;
+  //   event.target.note.value = "";
+  //   console.log(content);
+  // };
+
+  // const toggleImportance = (note) => {
+  //   console.log("toggle importance of", note.id);
+  // };
 
   return (
     <div>
